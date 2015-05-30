65d39a6ef5d09448ed2dc6d587a159dc9a0560d8
Henrique Nazaré <henry.nazare@gmail.com>
2015-05-30 20:43:44 -0300

    Add PropertyValue class

--- include/Properties.h
+++ include/Properties.h
@@ -2,3 +2,35 @@
 
 #include <PrologInterface.h>
 
+#include <iostream>
+#include <map>
+#include <vector>
+
+class PropertyValue {
+public:
+  PropertyValue();
+  PropertyValue(PrologTerm term);
+
+  PrologTerm term() const;
+
+  std::string uuid() const;
+
+protected:
+  class UUID {
+  public:
+    UUID();
+    UUID(std::string uuid);
+
+    std::string str() const;
+
+  private:
+    std::string uuid_;
+  };
+
+  PropertyValue(UUID uuid, PrologTerm term);
+
+private:
+  UUID uuid_;
+  PrologTerm term_;
+};
+
--- src/Properties.cpp
+++ src/Properties.cpp
@@ -1,2 +1,42 @@
 #include "Properties.h"
 
+/**
+ * PropertyValue
+ */
+static int next_uuid() {
+  static unsigned int current_uuid = 0;
+  return current_uuid++;
+}
+
+PropertyValue::UUID::UUID()
+    : UUID(std::to_string(next_uuid())) {
+}
+
+PropertyValue::UUID::UUID(std::string uuid)
+    : uuid_(uuid) {
+}
+
+std::string PropertyValue::UUID::str() const {
+  return uuid_;
+}
+
+PropertyValue::PropertyValue()
+    : uuid_(), term_(PrologString(uuid())) {
+}
+
+PropertyValue::PropertyValue(PrologTerm term)
+    : term_(term) {
+}
+
+PropertyValue::PropertyValue(UUID uuid, PrologTerm term)
+    : uuid_(uuid), term_(term) {
+}
+
+PrologTerm PropertyValue::term() const {
+  return term_;
+}
+
+std::string PropertyValue::uuid() const {
+  return uuid_.str();
+}
+
