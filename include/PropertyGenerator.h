265af6012f3dd706bf9f31512d3755a8dfb476dd
Henrique Nazaré <henry.nazare@gmail.com>
2015-05-30 21:29:44 -0300

    Add PropertyGenerator class

---
+++ include/PropertyGenerator.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Properties.h"
+
+#include <vector>
+#include <functional>
+
+template<typename T>
+class PropertyGenerator : public std::vector<T> {
+public:
+  PropertyGenerator() {}
+
+  template<typename U>
+  PropertyGenerator(U begin, U end, std::function<T (U)> conv) {
+    for (auto it = begin; it != end; ++it) {
+      this->push_back(conv(it));
+    }
+  }
+
+  template<typename U, typename I>
+  PropertyGenerator(U iterable, std::function<T (I)> conv) {
+    for (auto& it : iterable) {
+      this->push_back(conv(it));
+    }
+  }
+};
+
